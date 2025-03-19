ea3ae9d3b4853c1b6741777075f8206e547161a7
Joshua MARTINELLE <contact@jomar.fr>
2025-03-19 19:16:55 +0100

    Add simplecov-json

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-json'
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 SimpleCov.start do
   add_filter '/spec/'
