55b366696692082fcd02af18dda25b52037842bc
Joshua MARTINELLE <contact@jomar.fr>
2025-03-19 19:33:40 +0100

    Update test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@
 require 'simplecov'
 require 'simplecov-lcov'
 
+SimpleCov::Formatter::LcovFormatter.config.output_directory = 'coverage'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 
 SimpleCov.start do
