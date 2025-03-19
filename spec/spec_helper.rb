221d02c1d75df282390a3fc8da9d6e226fceaeb7
Joshua MARTINELLE <contact@jomar.fr>
2025-03-19 19:23:04 +0100

    Remove simplecov-json

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-json'
-
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 SimpleCov.start do
   add_filter '/spec/'
